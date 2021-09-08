f79db722740f4a64041e24f5a0e8e6773a6cf228
Praveen Singh <prrraveen@gmail.com>
2021-09-09 00:17:40 +0530

    closest pair

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -1,12 +1,22 @@
+import collections
+import math
 from typing import List
 
 from test_framework import generic_test
 
-
 def find_nearest_repetition(paragraph: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
-
+    """did it on my own"""
+    word_dict = {}
+    min_dist = math.inf
+    for i, word in enumerate(paragraph):
+        if word not in word_dict:
+            word_dict[word] = i
+        else:
+            min_dist = min(min_dist, i - word_dict[word])
+            word_dict[word] = i
+    if min_dist != math.inf:
+        return min_dist
+    return -1
 
 if __name__ == '__main__':
     exit(
