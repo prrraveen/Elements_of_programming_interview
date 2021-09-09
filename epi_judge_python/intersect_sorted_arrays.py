38d742145f98434f91796516c35dba40e5bbaf6a
Praveen Singh <prrraveen@gmail.com>
2021-09-09 19:30:56 +0530

    13.2 merge two sorted arrays

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -1,26 +1,20 @@
+import bisect
 import math
 from typing import List
 
 from test_framework import generic_test
 
 
-def intersect_two_sorted_arrays(xs: List[int], ys: List[int]) -> List[int]:
-    p1, p2 = 0, 0
-    last_added, result = math.inf, []
-    while p1 < len(xs) and p2 < len(ys):
-        xp1 = xs[p1]
-        yp2 = ys[p2]
-        if xp1 < yp2:
-            p1 += 1
-        elif xp1 > yp2:
-            p2 += 1
-        else:
-            if last_added != xp1:
-                result.append(xp1)
-            last_added = xp1
-            p1 += 1
-    return result
-    return
+def intersect_two_sorted_arrays(A: List[int], B: List[int]) -> List[int]:
+    def is_present(k):
+        i = bisect.bisect_left(B, k)
+        return i < len(B) and B[i] == k
+
+
+    return [
+        a for i, a in enumerate(A)
+        if (i == 0 or a != A[i - 1]) and is_present(a)
+    ]
 
 
 
--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 
 from test_framework import generic_test
@@ -5,8 +6,19 @@ from test_framework import generic_test
 
 def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
                             n: int) -> None:
-    # TODO - you fill in here.
-    return
+    a, b, write_idx = m - 1, n - 1, m + n - 1
+    while a >= 0 and b >= 0:
+        if A[a] > B[b]:
+            A[write_idx] = A[a]
+            a -= 1
+        else:
+            A[write_idx] = B[b]
+            b -= 1
+        write_idx -= 1
+
+    while b >= 0:
+        A[write_idx] = B[b]
+        write_idx, b = write_idx - 1, b - 1
 
 
 def merge_two_sorted_arrays_wrapper(A, m, B, n):
